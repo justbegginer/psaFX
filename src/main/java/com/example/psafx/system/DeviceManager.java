135afb2abf9a4ea885932c82878201dd3234aebd
justbegginer <blackcloudsinthesky@protonmail.com>
2023-12-10 10:06:31 +0300

    fix add principle

--- src/main/java/com/example/psafx/system/DeviceManager.java
+++ src/main/java/com/example/psafx/system/DeviceManager.java
@@ -1,8 +1,5 @@
 package com.example.psafx.system;
 
-import com.example.psafx.util.TimeScale;
-
-import javax.swing.text.html.Option;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -10,11 +7,14 @@ import java.util.Optional;
 
 public class DeviceManager {
 
+    private int currentDeviceNumber;
+
     private final double timeToFinishSeconds;
 
     private List<Device> deviceList;
 
     public DeviceManager(int deviceCount, double timeToFinishSeconds) {
+        this.currentDeviceNumber = 1;
         deviceList = new ArrayList<>(deviceCount);
         for (int i = 0; i < deviceCount; i++) {
             deviceList.add(new Device(i + 1, timeToFinishSeconds));
@@ -23,13 +23,26 @@ public class DeviceManager {
     }
 
     public Device addNewTask(Task task, double currentTime) {
-        for (Device device : deviceList) {
-            if (!device.isBusy()) {
-                device.addTask(task, currentTime);
-                return device;
+        for (int i = 0; i < deviceList.size(); i++) {
+            if (!deviceList.get(currentDeviceNumber - 1).isBusy()) {
+                Device tempDevice = deviceList.get(currentDeviceNumber - 1);
+                tempDevice.addTask(task, currentTime);
+                iterateCurrentDeviceNumber();
+                return tempDevice;
+            }
+            else {
+                iterateCurrentDeviceNumber();
             }
         }
         return null;
+    }
+
+    private void iterateCurrentDeviceNumber() {
+        if (currentDeviceNumber < deviceList.size()) {
+            currentDeviceNumber++;
+        } else {
+            currentDeviceNumber = 1;
+        }
     }
 
     public Double getClosestEvent() {
@@ -38,10 +51,9 @@ public class DeviceManager {
                 .filter(device -> device.getTimeScale().getEndTime() != null)
                 .filter(Device::isBusy)// фильтрация для исключения null значений
                 .min(Comparator.comparingDouble(device -> device.getTimeScale().getEndTime()));
-        if (deviceTemp.isPresent()){
+        if (deviceTemp.isPresent()) {
             return deviceTemp.get().getTimeScale().getEndTime();
-        }
-        else{
+        } else {
             return null;
         }
     }
@@ -49,25 +61,25 @@ public class DeviceManager {
     public Device popDevice() {
         double closestEvent = getClosestEvent();
         for (Device device : deviceList) {
-            if (device.isBusy() && device.getTimeScale().getEndTime() == closestEvent){
+            if (device.isBusy() && device.getTimeScale().getEndTime() == closestEvent) {
                 return device;
             }
         }
         return null;
     }
 
-    public boolean isDevicesBusy(){
+    public boolean isDevicesBusy() {
         for (Device device : deviceList) {
-            if(!device.isBusy()){
+            if (!device.isBusy()) {
                 return false;
             }
         }
         return true;
     }
 
-    public boolean isDevicesEmpty(){
-        for (Device device : deviceList){
-            if(device.isBusy()){
+    public boolean isDevicesEmpty() {
+        for (Device device : deviceList) {
+            if (device.isBusy()) {
                 return false;
             }
         }
